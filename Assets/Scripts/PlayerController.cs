a35693dba90bd95268164082ff1a9da99255804c
unknown <mj9617@IGMGRAD-12.main.ad.rit.edu>
2015-09-12 15:35:33 -0400

    Base

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    public float speed;
+    public float theta;
+    public float jump;
+    public float gravity;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -theta * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, theta * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.position += transform.forward * speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.position -= transform.forward * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            transform.position += transform.up * jump * Time.deltaTime;
+
+        if(transform.position.y > 0.5f)
+            transform.position -= transform.up * gravity * Time.deltaTime;
+
+        if (transform.position.y < 0.5f)
+            transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+    }
+}
